e00630f9cd5b34978b09a443fe3b137132a54923
John <johnmadigan82@gmail.com>
2018-01-17 20:50:19 -0500

    Make GamepadHelper static

--- src/org/pvcpirates/frc2018/util/GamepadHelper.java
+++ src/org/pvcpirates/frc2018/util/GamepadHelper.java
@@ -2,7 +2,7 @@ package org.pvcpirates.frc2018.util;
 
 public class GamepadHelper {
 
-    public double applyDeadBand(double initial, double deadBand){
+    public static double applyDeadBand(double initial, double deadBand){
         if(Math.abs(initial) < Math.abs(deadBand)){
             return 0;
         }else{
@@ -10,7 +10,7 @@ public class GamepadHelper {
         }
     }
 
-    public double mapToCurve(double initial, BezierCurve curve){
+    public static double mapToCurve(double initial, BezierCurve curve){
         return curve.getY(initial);
     }
 }
