259454f07166fe610ba5df14d50901fb92f15c0f
= <=>
2018-01-25 20:19:06 -0500

    Add Gravity Compensation

---
+++ src/org/pvcpirates/frc2018/autonomous/DriveUntil.java
@@ -0,0 +1,13 @@
+package org.pvcpirates.frc2018.autonomous;
+
+public class DriveUntil extends AutoSubCommand{
+
+	public DriveUntil(AutoCommand parent) {
+		super(parent);
+		// TODO Auto-generated constructor stub
+	}
+	public DriveUntil(AutoCommand parent, double inches){
+		super(parent);
+	}
+
+}
