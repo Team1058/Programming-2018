bcb1c7253312772219399aeafde2f25ffe39f902
samuelh-ta <frydthundor@gmail.com>
2018-03-07 16:05:44 -0500

    Fixed/Added to SwitchAuto

---
+++ src/org/pvcpirates/frc2018/commands/CubeGrab.java
@@ -0,0 +1,35 @@
+package org.pvcpirates.frc2018.commands;
+
+import org.pvcpirates.frc2018.Status;
+import org.pvcpirates.frc2018.robot.subsystems.Arm;
+import org.pvcpirates.frc2018.robot.subsystems.Grabber;
+
+public class CubeGrab extends Command {
+
+	public CubeGrab() {
+	}
+		@Override
+		public void init(){
+			
+			setStatus(Status.EXEC);
+		}
+		
+		@Override
+		public void exec() {
+			if (Arm.getWristAngle()<80) {
+			Grabber.intakeRollers();
+			Grabber.closeGrabber();
+			setStatus(Status.STOP);
+			while (Arm.getWristAngle()<80) {
+			commands.add(new DriveFor(6));	
+			}
+		}
+	}
+			
+			@Override
+			public void finished() {				
+			System.out.println("Picked");
+			
+			
+	}
+}
--- src/org/pvcpirates/frc2018/commands/SwitchAuto.java
+++ src/org/pvcpirates/frc2018/commands/SwitchAuto.java
@@ -1,6 +1,7 @@
 package org.pvcpirates.frc2018.commands;
 
 import org.pvcpirates.frc2018.autonomous.StartingLocation;
+import org.pvcpirates.frc2018.teleop.MoveToSetpoint;
 
 import edu.wpi.first.wpilibj.DriverStation;
 
@@ -25,26 +26,38 @@ public class SwitchAuto extends Command {
 		//CENTER START
 		if(location == StartingLocation.CENTER){
 			if(gameData.charAt(0) == 'L'){
-				commands.add(new DriveFor(30));
-				commands.add(new TurnToAngle(45));
-				commands.add(new DriveFor(83));
-				commands.add(new TurnToAngle(-45));
-				commands.add(new DriveFor(35));
+				commands.add(new TurnToAngle(20));
+				commands.add(new DriveFor(104));
+				commands.add(new TurnToAngle(-20));
+				commands.add(new DriveFor(1));
+				commands.add(new SafeMoveArmPolarSetpoint(0,-10));
+				commands.add(new SpitCube());
 			}else if(gameData.charAt(0) == 'R'){
-				commands.add(new DriveFor(30));
-				commands.add(new TurnToAngle(-45));
-				commands.add(new DriveFor(83));
-				commands.add(new TurnToAngle(45));
-				commands.add(new DriveFor(37));
+				commands.add(new TurnToAngle(-20));
+				commands.add(new DriveFor(104));
+				commands.add(new TurnToAngle(20));
+				commands.add(new DriveFor(1));
+				commands.add(new SafeMoveArmPolarSetpoint(0,-10));
+				commands.add(new SpitCube());
 			}
 		}
 
 		//RIGHT START
 		if(location == StartingLocation.RIGHT){
-			if(gameData.charAt(0) == 'R'){
-				commands.add(new DriveFor(172));
+			if(gameData.charAt(0) == 'R' && gameData.charAt(1)=='R'){
+				commands.add(new DriveFor(160));
+				commands.add(new TurnToAngle(-90));
+				commands.add(new DriveFor(41.125));
+				commands.add(new SafeMoveArmPolarSetpoint(0,-10));
+				commands.add(new SpitCube());
 				commands.add(new TurnToAngle(90));
-			}else if(gameData.charAt(0) == 'L'){
+				commands.add(new SafeMoveArmPolarSetpoint(22,-60));
+				commands.add(new DriveFor(32.5));
+				commands.add(new TurnToAngle(-90));
+				commands.add(new CubeGrab());
+				commands.add(new DriveFor(-82.65));
+				commands.add(new SafeMoveArmPolarSetpoint(31 , 97, -60));
+				}else if(gameData.charAt(0) == 'L'){
 				return;
 			}
 		}
