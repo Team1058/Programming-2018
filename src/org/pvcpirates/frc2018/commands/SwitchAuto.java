a07b9c94181c533634460f6761d84e290ae6ac4e
samuelh-ta <frydthundor@gmail.com>
2018-03-08 09:10:56 -0500

    Added to SwitchAuto (Error on line 60)

--- src/org/pvcpirates/frc2018/commands/SwitchAuto.java
+++ src/org/pvcpirates/frc2018/commands/SwitchAuto.java
@@ -45,6 +45,7 @@ public class SwitchAuto extends Command {
 		//RIGHT START
 		if(location == StartingLocation.RIGHT){
 			if(gameData.charAt(0) == 'R' && gameData.charAt(1)=='R'){
+				//Going for Switch and Scale if they are both on the right side
 				commands.add(new DriveFor(160));
 				commands.add(new TurnToAngle(-90));
 				commands.add(new DriveFor(41.125));
@@ -56,10 +57,21 @@ public class SwitchAuto extends Command {
 				commands.add(new TurnToAngle(-90));
 				commands.add(new CubeGrab());
 				commands.add(new DriveFor(-82.65));
-				commands.add(new SafeMoveArmPolarSetpoint(31 , 97, -60));
-				}else if(gameData.charAt(0) == 'L'){
-				return;
-			}
+				commands.add(new SafeMoveArmPolarSetpoint(31,97,-60));
+				commands.add(new SpitCube());
+				}else if(gameData.charAt(0) == 'R' && gameData.charAt(1)=='L'){
+				//Going for Switch on right and scale on left sides
+					commands.add(new DriveFor(160));
+					commands.add(new TurnToAngle(-90));
+					commands.add(new DriveFor(41.125));
+					commands.add(new SafeMoveArmPolarSetpoint(0,-10));
+					commands.add(new SpitCube());
+					commands.add(new TurnToAngle(90));
+					commands.add(new SafeMoveArmPolarSetpoint(22,-60));
+					commands.add(new DriveFor(32.5));
+					commands.add(new TurnToAngle(-90));
+					commands.add(new CubeGrab());
+					}else if (gameData.charAt(0) == 'L') {
 		}
 
 		if(location == StartingLocation.LEFT){
@@ -75,4 +87,5 @@ public class SwitchAuto extends Command {
 		commands.add(new WristRotate(90));
 		commands.add(new SpitCube());
 	}
+}
 }
